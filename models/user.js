bcf9259bb63aa62cc5f86d2e8589aabce2cfe591
Bahare Mohajeri <Baharemohajeri94@gmail.com>
2025-01-05 21:39:26 +0330

    create user model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    username:{
+        type : String,
+        required : true,
+    },
+    email:{
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password:{
+        type : String,
+        required : true,
+    },
+    phonenumber:{
+        type : String,
+        required : true,
+    },
+    name:{
+        type : String,
+        required : true,
+    },
+    role:{
+        type : String,
+        enum : ["ADMIN", "USER"],
+        default : 'USER',
+        required : true,
+    },
+
+}, {Timestamp: true } )
+
+new model = mongoose.model ( 'User' , schema ) ; 
+module.exports = model ; 
